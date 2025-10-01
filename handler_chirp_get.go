d03ce10ce8dadffb6c7a1711ce81b36a2d1c7716
jerslf <jerslf@hotmail.co.uk>
2025-10-01 21:52:25 +0200

    Get Chirp

--- handler_chirp_get.go
+++ handler_chirp_get.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"database/sql"
+	"errors"
 	"net/http"
 
+	"github.com/google/uuid"
 	"github.com/jerslf/chirpy/internal/database"
 )
 
@@ -14,6 +17,38 @@ func (cfg *apiConfig) handlerGetChirps(w http.ResponseWriter, r *http.Request) {
 	}
 
 	respondWithJSON(w, http.StatusOK, dbChirpsToAPI(chirps))
+}
+
+func (cfg *apiConfig) handlerGetChirp(w http.ResponseWriter, r *http.Request) {
+	chirpIDStr := r.PathValue("chirpID")
+	chirpID, err := uuid.Parse(chirpIDStr)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid chirp ID", err)
+		return
+	}
+
+	chirp, err := cfg.db.GetChirpByID(r.Context(), chirpID)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			respondWithError(w, http.StatusNotFound, "Chirp not found", nil)
+		} else {
+			respondWithError(w, http.StatusInternalServerError, "Couldn't get chirp", err)
+		}
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, dbChirpToAPI(chirp))
+
+}
+
+func dbChirpToAPI(c database.Chirp) Chirp {
+	return Chirp{
+		ID:        c.ID,
+		CreatedAt: c.CreatedAt,
+		UpdatedAt: c.UpdatedAt,
+		Body:      c.Body,
+		UserID:    c.UserID,
+	}
 }
 
 func dbChirpsToAPI(dbChirps []database.Chirp) []Chirp {
